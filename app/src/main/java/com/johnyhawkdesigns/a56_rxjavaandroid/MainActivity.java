b398fec6cb5c03863b1f7bb902ef306ba2d07a8e
Ahsan <johnyhawkahsan@gmail.com>
2020-01-14 11:53:04 +0500

    Interval + Timer Operators

--- app/src/main/java/com/johnyhawkdesigns/a56_rxjavaandroid/MainActivity.java
+++ app/src/main/java/com/johnyhawkdesigns/a56_rxjavaandroid/MainActivity.java
@@ -14,8 +14,11 @@ import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,193 +35,57 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textView1);
 
-        // This is a single task because we want to test "create" operator which only accepts one task
-        final Task task = new Task("Walk the dog", false, 3);
+
+//===================================Using "Handler" and "Runnable" Operator for DELAY================================//
+//A pretty common problem we Android programmers run into is 'running a method' or 'executing some kind of process' at a specific time interval or delay.
+
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+
+            int elapsedTime = 0;
+
+            @Override
+            public void run() {
+                if (elapsedTime >= 5) { // if greater than 5 seconds
+                    handler.removeCallbacks(this); // remove callback
+                } else {
+                    elapsedTime = elapsedTime + 1; // As long as less than "5", increment by 1
+                    handler.postDelayed(this, 1000);
+                    Log.d(TAG, "run: elapsedTime = " + elapsedTime);
+                }
+            }
+        };
+
+        //Typically, delay is implemented using a Runnable and a Handler with a postDelayed() method call.
+        handler.postDelayed(runnable, 1000); // 1 sec delay
 
 
-        //======================================JUST Operator===================================//
-        // We are using "create" operator
-        Observable<Task> createObservable = Observable
-                //The Create() operator is used to create Observables. The 'just()' and 'create()' operators should be used if you want to create a single Observable. The Just() operator has the ability to accept a list of up to 10 entries.
-                .create(new ObservableOnSubscribe<Task>() {
+        //======================================interval() Operator===================================//
+
+
+        // We are using "interval" operator = emit an observable every time interval
+        Observable<Long> intervalObservable = Observable
+                .interval(1, TimeUnit.SECONDS) // For "1" second
+                .subscribeOn(Schedulers.io()) // We want it to work on background thread
+                .takeWhile(new Predicate<Long>() { // stop the process if more than 5 seconds passes
                     @Override
-                    public void subscribe(ObservableEmitter<Task> emitter) throws Exception {
-
-
-                     /*
-                        // If we want to use single task in "create" operator
-                        if (!emitter.isDisposed()) { // check if emitter is not disposed
-                            emitter.onNext(task); // just initiate single task
-                            emitter.onComplete(); // call complete method to finish emitter
-                        }
-                     */
-
-                        // we use for loop if we want multiple tasks instead of single task in "create" operator
-                        for (Task task : DataSource.createTasksList()) { // creating list of Tasks
-                            if (!emitter.isDisposed()) {
-                                emitter.onNext(task);
-                            }
-                        }
-
-                        if (!emitter.isDisposed()) {
-                            emitter.onComplete(); // call complete method to finish emitter
-                        }
-
-                    }
-                })
-                .subscribeOn(Schedulers.io()) // We want it to work on background thread
-                .observeOn(AndroidSchedulers.mainThread()); // We want to show results on main thread
-
-        createObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: called.");
-            }
-
-            @Override
-            public void onNext(Task task) {
-                Log.d(TAG, "onNext: ThreadName = " + Thread.currentThread().getName());
-                Log.d(TAG, "onNext: " + task.getDescription());
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "onError: ", e);
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete: called.");
-            }
-        });
-
-
-
-        //======================================JUST Operator===================================//
-        // Example of just operator = In this example I'm using the "just()" operator and passing a data set of --10-- Strings.
-        Observable<Task> justObservable = Observable
-                //The Create() operator is used to create Observables. The 'just()' and 'create()' operators should be used if you want to create a single Observable. The Just() operator has the ability to accept a list of up to 10 entries.
-                .just(task)
-                .subscribeOn(Schedulers.io()) // We want it to work on background thread
-                .observeOn(AndroidSchedulers.mainThread()); // We want to show results on main thread
-
-        justObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: called.");
-            }
-
-            @Override
-            public void onNext(Task task) {
-                Log.d(TAG, "onNext: ThreadName = " + Thread.currentThread().getName());
-                Log.d(TAG, "onNext: " + task.getDescription());
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "onError: ", e);
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete: called.");
-            }
-        });
-
-
-
-        //======================================Range Operator===================================//
-        // We are using "range" operator
-        Observable<Integer> rangeObservable = Observable
-                .range(0, 9)
-                .subscribeOn(Schedulers.io()) // We want it to work on background thread
-                .observeOn(AndroidSchedulers.mainThread()); // We want to show results on main thread
-
-        rangeObservable.subscribe(new Observer<Integer>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: called.");
-            }
-
-            @Override
-            public void onNext(Integer integer) {
-                Log.d(TAG, "onNext: " + integer);
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "onError: ", e);
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete: called.");
-            }
-        });
-
-        // We are using "range" operator with MAP
-        Observable<Task> rangeObservableWithMap = Observable
-                .range(0, 9)
-                .subscribeOn(Schedulers.io()) // We want it to work on background thread
-                .map(new Function<Integer, Task>() {
-                    @Override
-                    public Task apply(Integer integer) throws Exception {
-                        Log.d(TAG, "apply: " + Thread.currentThread().getName());
-                        return new Task("This is task with priority: " + String.valueOf(integer),
-                                false,
-                                integer);
-                    }
-                })
-                .takeWhile(new Predicate<Task>() {
-                    @Override
-                    public boolean test(Task task) throws Exception {
-                        return task.getPriority() < 9;
+                    public boolean test(Long aLong) throws Exception {
+                        Log.d(TAG, "test: aLong = " + aLong + ", thread name: " + Thread.currentThread().getName());
+                        return aLong <= 5;
                     }
                 })
                 .observeOn(AndroidSchedulers.mainThread()); // We want to show results on main thread
 
-
-        rangeObservableWithMap.subscribe(new Observer<Task>() {
+        //Subscribe to the Observable:
+        intervalObservable.subscribe(new Observer<Long>() {
             @Override
             public void onSubscribe(Disposable d) {
                 Log.d(TAG, "onSubscribe: called.");
             }
 
             @Override
-            public void onNext(Task task1) {
-                Log.d(TAG, "onNext: " + task1.getPriority());
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "onError: ", e);
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete: called.");
-            }
-        });
-
-
-        //======================================Repeat Operator===================================//
-        // We are using "range" operator
-        Observable<Integer> repeatObservable = Observable
-                .range(0, 3)
-                .subscribeOn(Schedulers.io()) // We want it to work on background thread
-                .repeat(3) // repeat 3 times
-                .observeOn(AndroidSchedulers.mainThread()); // We want to show results on main thread
-
-
-        repeatObservable.subscribe(new Observer<Integer>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: called.");
-            }
-
-            @Override
-            public void onNext(Integer integer) {
-                Log.d(TAG, "onNext: " + integer);
+            public void onNext(Long aLong) {
+                Log.d(TAG, "onNext: interval: " + aLong);
             }
 
             @Override
@@ -234,7 +101,34 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        //======================================timer() Operator===================================//
+        // emit single observable after a given delay
+        Observable<Long> timeObservable = Observable
+                .timer(3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
 
+        timeObservable.subscribe(new Observer<Long>() {
+
+            long time = 0; // variable for demonstrating how much time has passed
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                time = System.currentTimeMillis() / 1000;
+            }
+            @Override
+            public void onNext(Long aLong) {
+                Log.d(TAG, "onNext: " + ((System.currentTimeMillis() / 1000) - time) + " seconds have elapsed." );
+            }
+            @Override
+            public void onError(Throwable e) {
+
+            }
+            @Override
+            public void onComplete() {
+
+            }
+        });
 
 
     }
