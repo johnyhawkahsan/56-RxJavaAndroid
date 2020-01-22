1aa95df8eba2c5ee550a081f12b653446c1ffc86
Ahsan <johnyhawkahsan@gmail.com>
2020-01-22 11:00:12 +0500

    09-Buffers - Simple example + RxBinding library

--- app/src/main/java/com/johnyhawkdesigns/a56_rxjavaandroid/MainActivity.java
+++ app/src/main/java/com/johnyhawkdesigns/a56_rxjavaandroid/MainActivity.java
@@ -13,18 +13,23 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
+
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
+import kotlin.Unit;
 import okhttp3.ResponseBody;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding3.view.RxView;
 import com.johnyhawkdesigns.a56_rxjavaandroid.MVVM.MainViewModel;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     // ui
     private TextView textView;
 
+    // global disposables object
+    CompositeDisposable disposables;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,82 +50,81 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView1);
 
 
-        // Transformation Operators - Map
-        // map = "Applies a function to each emitted item. It transforms each emitted item by applying a function to it."
-        //====================================== Mapping (Task -> String) ===================================//
-        Observable<String> extractDescriptionObservable = Observable
+        // Transformation Operators - Buffer
+        // buffer = "Periodically divide/gather items from an Observable into bundles(groups) and emit the bundles(groups) rather than emitting items one at a time."
+        //====================================== Buffer - Simple example ===================================//
+        Observable<Task> bufferObservable = Observable
                 .fromIterable(DataSource.createTasksList())
                 .subscribeOn(Schedulers.io())
-                .map(extractDescriptionFunction) // map = Returns an Observable that applies a specified function to each item emitted by the source ObservableSource and emits the results of these function applications.
                 .observeOn(AndroidSchedulers.mainThread());
 
-        extractDescriptionObservable.subscribe(new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-            }
-            @Override
-            public void onNext(String s) {
-                Log.d(TAG, "onNext: extracted description: " + s);
-            }
-            @Override
-            public void onError(Throwable e) {
-            }
-            @Override
-            public void onComplete() {
-            }
-        });
+        bufferObservable
+                .buffer(2) // Apply the Buffer() operator so it may group 2 items
+                .subscribe(new Observer<List<Task>>() { // Subscribe and view the emitted results
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+                    @Override
+                    public void onNext(List<Task> tasks) {
+                        Log.d(TAG, "onNext: bundle results: -------------------");
+                        for(Task task: tasks){
+                            Log.d(TAG, "onNext: " + task.getDescription());
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                    }
+                    @Override
+                    public void onComplete() {
+                    }
+                });
 
 
+        // Another very useful application is tracking UI interactions. I'll also show you how to use a very helpful library made by Jake Wharton, the RxBinding library. We can use the RxBinding library to make click events observable.
+        //====================================== Buffer - Tracking UI Interactions ===================================//
+        disposables = new CompositeDisposable();
 
+        // detect clicks to a button
+        RxView.clicks(findViewById(R.id.button))
+                .map(new Function<Unit, Integer>() { // convert the detected clicks to an integer
+                    @Override
+                    public Integer apply(Unit unit) throws Exception {
+                        return 1; // one click returns 1 integer
+                    }
+                })
+                .buffer(4, TimeUnit.SECONDS)  // capture all the clicks during a 4 second interval
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<Integer>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposables.add(d); // add to disposables to you can clear in onDestroy
+                    }
+                    @Override
+                    public void onNext(List<Integer> integers) {
+                        Log.d(TAG, "onNext: You clicked " + integers.size() + " times in 4 seconds!");
+                    }
 
-        // In this example I will create a custom map function that updates a Task object and then emits that updated Task.
-        //====================================== Mapping (Task -> Updated Task) ===================================//
-        Observable<Task> completeTaskObservable = Observable
-                .fromIterable(DataSource.createTasksList())
-                .subscribeOn(Schedulers.io())
-                .map(completeTaskFunction)
-                .observeOn(AndroidSchedulers.mainThread());
+                    @Override
+                    public void onError(Throwable e) {
 
-        completeTaskObservable.subscribe(new Observer<Task>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-            }
-            @Override
-            public void onNext(Task task) {
-                Log.d(TAG, "onNext: is this task complete? " + task.isComplete());
-            }
-            @Override
-            public void onError(Throwable e) {
-            }
-            @Override
-            public void onComplete() {
-            }
-        });
+                    }
 
+                    @Override
+                    public void onComplete() {
 
+                    }
+                });
 
 
     }
 
-    // Function (RxJava) = A functional interface that takes a value and returns another value, possibly with a different type
-    // This is a function to extract description from task.
-    Function<Task, String> extractDescriptionFunction = new Function<Task, String>() {
-        @Override
-        public String apply(Task task) throws Exception {
-            Log.d(TAG, "apply: doing work on thread: " + Thread.currentThread().getName());
-            return task.getDescription();
-        }
-    };
 
-
-    // Receives a Task object and modifies it and then emits that updated Task.
-    Function<Task, Task> completeTaskFunction = new Function<Task, Task>() {
-        @Override
-        public Task apply(Task task) throws Exception {
-            Log.d(TAG, "apply: doing work on thread: " + Thread.currentThread().getName());
-            task.setComplete(true);
-            return task;
-        }
-    };
+    // make sure to clear disposables when the activity is destroyed
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposables.clear();
+    }
 }
 
